6ba4cc4800cc9919538d2ebd96d83e22cbeaf744
juerui <juerui@vip.qq.com>
2017-06-28 15:17:39 +0800

    需求1--列表渲染

--- js/tdlist.js
+++ js/tdlist.js
@@ -1,1 +1,16 @@
+// 需求1:列表渲染
+var list = [
+    {
+        title:"吃饭睡觉打豆豆"
+    },
+    {
+        title:"妙味课堂todolist"
+    }
+]
 
+new Vue({
+    el:".main",
+    data:{
+        tdlist:list
+    }
+})
