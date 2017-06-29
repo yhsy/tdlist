07093e74f5b98fd9d8b249f322ccaf9af9dca41b
juerui <juerui@vip.qq.com>
2017-06-29 09:27:36 +0800

    需求3--显示隐藏任务提示

--- js/tdlist.js
+++ js/tdlist.js
@@ -1,11 +1,11 @@
 // 需求1:列表渲染
 var list = [
-    {
-        title:"吃饭睡觉打豆豆"
-    },
-    {
-        title:"妙味课堂todolist"
-    }
+    // {
+    //     title:"吃饭睡觉打豆豆"
+    // },
+    // {
+    //     title:"妙味课堂todolist"
+    // }
 ]
 
 new Vue({
