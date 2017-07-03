45e163082a988bee85a6e23df24ca40685313192
juerui <juerui@vip.qq.com>
2017-07-03 18:58:08 +0800

    需求8--利用本地存储localStorage,深度监控数据

--- js/tdlist.js
+++ js/tdlist.js
@@ -1,15 +1,34 @@
 // 需求1:列表渲染
-var list = [
-    // {
-    //     title:"吃饭睡觉打豆豆",
-    //     // 选中状态
-    //     isChecked:false
-    // },
-    // {
-    //     title:"妙味课堂todolist",
-    //     isChecked:true
-    // }
-]
+// var list = [
+//     // {
+//     //     title:"吃饭睡觉打豆豆",
+//     //     // 选中状态
+//     //     isChecked:false
+//     // },
+//     // {
+//     //     title:"妙味课堂todolist",
+//     //     isChecked:true
+//     // }
+// ]
+
+// 需求8:利用本地存储localstorage,深度监控数据
+
+// 需求8:①.本地存储函数
+var store = {
+    // 储存数据
+    save(key,value){
+        // 存数据的时候,要把值转成json字符串
+        localStorage.setItem(key,JSON.stringify(value));
+    },
+    // 获取数据
+    fetch(key){
+        // 如果有数据则取数据(转成json数据对象),没数据则取空数组
+        return JSON.parse(localStorage.getItem(key)) || []
+    }
+}
+
+// 需求8:②.获取本地存取的任务列表(todo),默认没数据就为空数组(替换假数据为本地数据)
+var list = store.fetch("todo");
 
 new Vue({
     el:".main",
@@ -21,6 +40,22 @@ new Vue({
         etodo:"",
         // 需求6:⑥.编辑任务--记录编辑前的任务标题
         beforeTitle:""
+    },
+    // 需求8:③.监控数据--存取到本地数据库
+    watch:{
+        // 浅监控,只能监控tdlist本身的变化,不能监控tdlist内部的数据对象变化(例如标题,选中状态)
+        // tdlist:function(){
+        //     store.save("todo",this.tdlist);
+        // }
+
+        // 需求8:④.深度监控数据--存取数据库(含选中状态)
+        // 深度监控:不仅能监控tdlist本身变化,也能监控tdlist内部数据的变化
+        tdlist:{
+            handler:function(){
+                store.save("todo",this.tdlist)
+            },
+            deep:true
+        }
     },
     // 需求7:统计未完成任务数
     computed:{
@@ -90,4 +125,5 @@ new Vue({
             }
         }
     }
+
 })
