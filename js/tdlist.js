335dea08a1b53475a7d170783661d6467aa0845a
juerui <juerui@vip.qq.com>
2017-07-04 19:39:49 +0800

    根据hash值(all/unfinished/finished),筛选任务完成情况(所有任务/未完成任务/已完成任务)

--- js/tdlist.js
+++ js/tdlist.js
@@ -11,26 +11,45 @@
 //     // }
 // ]
 
-// 需求8:利用本地存储localstorage,深度监控数据
+// 需求8:利用本地存储localStorage(替换模拟假数据),深度监控数据
 
-// 需求8:①.本地存储函数
+// 需求8:①.创建本地存储函数
 var store = {
-    // 储存数据
-    save(key,value){
-        // 存数据的时候,要把值转成json字符串
-        localStorage.setItem(key,JSON.stringify(value));
+    // 存储到本地数据库
+    save:function(key,value){
+        localStorage.setItem(key,JSON.stringify(value))
     },
-    // 获取数据
-    fetch(key){
-        // 如果有数据则取数据(转成json数据对象),没数据则取空数组
+    // 读取本地数据库
+    fetch:function(key){
         return JSON.parse(localStorage.getItem(key)) || []
     }
 }
 
-// 需求8:②.获取本地存取的任务列表(todo),默认没数据就为空数组(替换假数据为本地数据)
+// 需求8:②.取得本地数据库的任务列表
 var list = store.fetch("todo");
 
-new Vue({
+// 需求9:⑥.删选函数优化代码,把筛选函数从计算函数里提出来,这样只有使用时才调用,不用在计算属性里,每次都执行
+var filtered = {
+    // 全部任务(返回任务列表)
+    all:function(list){
+        return list;
+    },
+    // 未完成任务(返回任务列表里的未完成任务集合,既isChecked:false)
+    unfinished:function(list){
+        return list.filter(function(item){
+            return !item.isChecked
+        })
+    },
+    // 已完成任务(返回已完成任务列表,既isChecked:true)
+    finished:function(list){
+        return list.filter(function(item){
+            return item.isChecked
+        })
+    }
+}
+
+// 需求9:④.通过hash值,过滤任务列表数据--vm实例化
+var vm = new Vue({
     el:".main",
     data:{
         tdlist:list,
@@ -39,21 +58,23 @@ new Vue({
         // 需求6:②编辑任务--正在编辑的任务内容
         etodo:"",
         // 需求6:⑥.编辑任务--记录编辑前的任务标题
-        beforeTitle:""
+        beforeTitle:"",
+        // 需求9:③.hash状态值存放,默认状态是all
+        visibility:"all"
     },
-    // 需求8:③.监控数据--存取到本地数据库
     watch:{
-        // 浅监控,只能监控tdlist本身的变化,不能监控tdlist内部的数据对象变化(例如标题,选中状态)
+        // 需求8:
+        // 浅监控:只能监控存储tdlist本身,不能监控存储tdlist里面的状态
         // tdlist:function(){
-        //     store.save("todo",this.tdlist);
+        //     store.save("todo",this.tdlist)
         // }
 
-        // 需求8:④.深度监控数据--存取数据库(含选中状态)
-        // 深度监控:不仅能监控tdlist本身变化,也能监控tdlist内部数据的变化
+        // 深度监控:监控存储tdlist本身,而且监控存储tdlist内部的数据对象
         tdlist:{
             handler:function(){
                 store.save("todo",this.tdlist)
             },
+            // 深度监控(是)
             deep:true
         }
     },
@@ -63,7 +84,41 @@ new Vue({
             return this.tdlist.filter(function(item){
                 return !item.isChecked
             }).length
+        },
+        // 需求9:④.通过hash值,过滤任务列表数据
+        hashFilterList:function(){
+            /*
+                需求9:④.通过hash值,过滤任务列表数据 -- 筛选函数
+                var filtered = {
+                    // 全部任务(返回任务列表)
+                    all:function(list){
+                        return list;
+                    },
+                    // 未完成任务(返回任务列表里的未完成任务集合,既isChecked:false)
+                    unfinished:function(list){
+                        return list.filter(function(item){
+                            return !item.isChecked
+                        })
+                    },
+                    // 已完成任务(返回已完成任务列表,既isChecked:true)
+                    finished:function(list){
+                        return list.filter(function(item){
+                            return item.isChecked
+                        })
+                    }
+                }
+            */
+
+            // 需求9:④.根据hash值,执行对应的状态函数(all/unfinished/finished),返回筛选后的任务数据列表
+            // return filtered[this.visibility](list);
+
+            /*
+                需求9:⑤.如果有hash值,那么返回对应的任务列表数据,
+                        如果是没有任务状态(例如#123,#aa),那就显示默认数据list,
+            */
+            return filstered[this.visibility] ? filtered[this.visibility](list) : list
         }
+        
     },
     methods:{
         // 需求2:添加任务--函数
@@ -125,5 +180,25 @@ new Vue({
             }
         }
     }
+})
 
+// 需求9:②.获取hash值函数
+function getHash(){
+    // 默认的hash值,#hash
+    var urlHash = window.location.hash;
+    // 截取掉前面的#号
+    var hash = urlHash.slice(1);
+    console.log(hash);
+    return hash;
+}
+
+//  默认载入执行一次
+getHash();
+
+// 需求9:根据hash值,筛选任务完成情况(所有任务/未完成任务/已完成任务)
+// 需求9:①.监听hash值的变化,获取当前的hash值
+window.addEventListener("hashchange",function(){
+    // getHash();  
+    // 需求9:④.通过hash值,过滤任务列表数据 -- 根据hash值的变化,改变visibility的值
+    vm.visibility = getHash();
 })
